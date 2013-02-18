f326caebebd37adab7f1360a3736cc7d7910f118
Ola Sundell <ola.sundell@gmail.com>
2013-02-18 11:05:33 +0100

    Added a webapp module

--- api/src/test/java/se/svt/api/ItemToXmlTest.java
+++ api/src/test/java/se/svt/api/ItemToXmlTest.java
@@ -1,13 +1,25 @@
 package se.svt.api;
 
+import neo.xredsys.api.Article;
+import neo.xredsys.content.type.ArticleType;
 import org.junit.Assert;
 import org.junit.Test;
 import se.svt.test.stubs.StubArticleBuilder;
 
 public class ItemToXmlTest {
+	private static final String TITLE = "title";
+	private static final int ID = 123;
+	private static final String ARTICLE_TYPE = "news";
+
 	@Test
 	public void shouldProduceXml() {
-		String xml = new ItemToXml().convert(new StubArticleBuilder().build());
+		final Article article = new StubArticleBuilder()
+				.setArticleType(ARTICLE_TYPE)
+				.setId(ID)
+				.setTitle(TITLE)
+				.build();
+
+		String xml = new ItemToXml().convert(article);
 
 		Assert.assertNotNull(xml);
 	}
