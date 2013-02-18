58d85950a5eb7bee864b9abc7b3261073613a9a0
Ola Sundell <ola.sundell@gmail.com>
2013-02-18 11:29:26 +0100

    Added a controller and a working project configuration

--- api/src/main/java/se/svt/api/controller/ApiController.java
+++ api/src/main/java/se/svt/api/controller/ApiController.java
@@ -1,10 +1,17 @@
 package se.svt.api.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * TODO write comment
  */
 @Controller
 public class ApiController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public final ModelAndView displayPage() {
+		return new ModelAndView();
+	}
 }
