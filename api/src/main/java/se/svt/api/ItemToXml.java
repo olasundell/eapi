d59ff7202b078b9842de6fe56d78fea69e54893f
Ola Sundell <ola.sundell@gmail.com>
2013-02-18 10:38:13 +0100

    Added ItemToJson, added .idea ignores to .gitignore

--- api/src/main/java/se/svt/api/ItemToXml.java
+++ api/src/main/java/se/svt/api/ItemToXml.java
@@ -1,7 +1,12 @@
 package se.svt.api;
 
+import neo.xredsys.api.Article;
+
 /**
  * TODO write comment
  */
 public class ItemToXml {
+	public String convert(Article article) {
+		return "";
+	}
 }
--- api/src/test/java/se/svt/api/ItemToXmlTest.java
+++ api/src/test/java/se/svt/api/ItemToXmlTest.java
@@ -1,9 +1,14 @@
 package se.svt.api;
 
+import org.junit.Assert;
 import org.junit.Test;
+import se.svt.test.stubs.StubArticleBuilder;
 
 public class ItemToXmlTest {
 	@Test
 	public void shouldProduceXml() {
+		String xml = new ItemToXml().convert(new StubArticleBuilder().build());
+
+		Assert.assertNotNull(xml);
 	}
 }
