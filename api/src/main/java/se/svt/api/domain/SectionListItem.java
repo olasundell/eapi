97dee4d30a4cf40a8950d87879252e4bedb7b2c6
rickard-svti <rickard.andersson@svt.se>
2013-02-19 13:01:53 +0100

    Section API

--- api/src/main/java/se/svt/api/domain/SectionListItem.java
+++ api/src/main/java/se/svt/api/domain/SectionListItem.java
@@ -2,12 +2,24 @@ package se.svt.api.domain;
 
 public class SectionListItem {
 	private final String name;
+	private final String url;
+	private final String contentUrl;
 
-	public SectionListItem(String name) {
+	public SectionListItem(String name, String url, String contentUrl) {
 		this.name = name;
+		this.url = url;
+		this.contentUrl = contentUrl;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getContentUrl() {
+		return contentUrl;
 	}
 }
